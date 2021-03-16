cfbd0cbf20eb4375caaf19e1d393b1e791c1eaa0
Laura Bustamante <laurabustamante@Lauras-2017-MacBook-Pro.local>
2021-03-16 10:38:12 -0400

    confirmed api search on add drink page

--- src/components/AddDrink.jsx
+++ src/components/AddDrink.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 
 class AddDrink extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+          baseURL: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=',
+          drinkName: '',
+          searchURL: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange (event) {
+        this.setState({ [event.target.id]: event.target.value })
+    }
+
+    handleSubmit (event) {
+        event.preventDefault()
+        this.setState({
+          searchURL: this.state.baseURL + this.state.drinkName
+        })
+      }
+
     render () {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     {/* text input to search API for drink */}
-                    <label htmlFor="searchInput">label</label>
-                    <input type="search" name="searchInput"/>
+                    <label htmlFor="drinkName">label</label>
+                    <input 
+                        id='drinkName'
+                        type="search" 
+                        value={this.state.drinkName}
+                        onChange={this.handleChange}
+                    />
                     {/* submit button */}
-                    <input type="submit" value="Search"/>
+                    <input 
+                        type="submit" 
+                        value="Search"
+                    />
                 </form>
-                {/*  */}
+                {/* render search results */}
+                <a href={this.state.searchURL}>{this.state.searchURL}</a>
             </div>
         )
     }
